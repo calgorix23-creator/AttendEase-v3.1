827c80cf96d64c907df0953e54e7edbae36af9d5
Andrea Ng <andreanzy26@gmail.com>
2026-01-01 17:21:57 +0800

    Build Tailwind CSS locally and link output

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+ content: [
+ './dist/index.html', // Add all HTML/JS/TS files that use Tailwind classes
+ './src/*/.{js,ts,jsx,tsx,html}', // Uncomment if you use a src folder
+ ],
+ theme: {
+ extend: {},
+ },
+ plugins: [],
+}
